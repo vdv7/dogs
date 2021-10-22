e42469d7845d8284c4d0dea2aee4bcbef46a45cb
vdv7 <vdv7@users.noreply.github.com>
2021-10-21 21:21:01 -0400

    working v1

---
+++ main.js
@@ -0,0 +1,37 @@
+const $ = document.querySelector.bind(document);
+
+function showImage(event){
+    var breed = this.innerText;
+    var priorSelected = $('.selected');
+    if(priorSelected){
+        priorSelected.className = '';
+    }
+    eiekccfnuunhlljvfufcnkubhldlngifvgccnrifhklf
+    .classList.add('selected');
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then( r=>r.json() )
+    .then( data => {
+        // console.log( data.message )
+        $('#dog').src = data.message;
+    });
+}
+
+
+
+function createButton( txt ){
+    var btn = document.createElement('button');
+    btn.innerText = txt;
+    $('#buttons').appendChild(btn);
+    btn.onclick = showImage;
+}
+
+window.onload = function(){
+    // $('.buttons').innerHTML = 'hey now'
+    this.fetch('https://dog.ceo/api/breeds/list/all')
+    .then(r=>r.json())
+    .then(data => {
+        // console.log ( data )
+        Object.keys( data.message )
+            .forEach( createButton );
+    })
+}
